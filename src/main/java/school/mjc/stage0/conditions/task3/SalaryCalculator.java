01fd0e79c5dc85506e9797d17fd67d11d2700214
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-22 14:21:15 +0500

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double after_taxes;
+        if (salary<=10000 && salary>0){
+            after_taxes = salary * 0.85;
+            System.out.println(after_taxes);
+        }else if (salary>10000 && salary<=20000){
+            after_taxes = salary * 0.82;
+            System.out.println(after_taxes);
+        }else if (salary>20000){
+            after_taxes = salary * 0.80;
+            System.out.println(after_taxes);
+        }else{
+            System.out.println("wrong input!");
+        }
     }
 }
