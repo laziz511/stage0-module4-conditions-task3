9229604c153190b0b2585cc60e7625b56d98d524
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-22 15:29:37 +0500

    Update AliquotNumbers.java

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
-      //  if(second%first ==0){
+        if(second%first ==0){
             System.out.println("Aliquot");
-      //  }else{
-       //     System.out.println("Not aliquot");
-       // }
+        }else{
+            System.out.println("Not aliquot");
+        }
     }
 }
